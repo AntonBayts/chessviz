91a6ba59313eb21a80bf3e914c0d6c5eb9ff53a4
AntonBayts <lokatheman@yandex.ru>
2019-03-13 10:34:55 +0700

    Create chessviz.cpp

---
+++ chessviz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "9|R H B Q K B H R|" << endl;
+    cout << "8|P P P P P P P P|" << endl;
+    cout << "7|               |" << endl;
+    cout << "6|               |" << endl;
+    cout << "5|               |" << endl;
+    cout << "4|               |" << endl;
+    cout << "3|               |" << endl;
+    cout << "2|P P P P P P P P|" << endl;
+    cout << "1|R H B Q K B H R|" << endl;
+    cout << "  a b c d e f g h " << endl;
+    return 0;
