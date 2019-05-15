1c820e11a17ff891974227cab8591ba4a667b6e3
AntonBayts <lokatheman@yandex.ru>
2019-05-15 20:49:46 +0700

    Edit Makefile

--- Makefile
+++ Makefile
@@ -1,20 +1,34 @@
-all:main
-main: ./build/main.o ./build/board.o ./build/board_print_plain.o
-	gcc -o ./bin/main ./build/main.o ./build/board.o ./build/board_print_plain.o -Wall -Werror
+all: bin/chessviz bin/chessviz-test
 
-./build/main.o: ./src/main.c
-	gcc -o ./build/main.o -c ./src/main.c -Wall -Werror
+bin/chessviz: build/src/main.o build/src/board_print_plain.o build/src/board.o build/src/board_read.o
+	gcc -Wall -Werror build/src/main.o build/src/board_print_plain.o build/src/board.o build/src/board_read.o -o bin/chessviz
 
-./build/board_print_plain.o: ./src/board_print_plain.c
-	gcc -o ./build/board_print_plain.o -c ./src/board_print_plain.c -Wall -Werror
+build/src/main.o: src/main.c
+	gcc -Wall -Werror -c src/main.c -o build/src/main.o
 
-./build/board.o: ./src/board.c
-	gcc -o ./build/board.o -c ./src/board.c -Wall -Werror
+build/src/board_print_plain.o: src/board_print_plain.c
+	gcc -Wall -Werror -c src/board_print_plain.c -o build/src/board_print_plain.o
+
+build/src/board.o: src/board.c
+	gcc -Wall -Werror -c src/board.c -o build/src/board.o
+
+build/src/board_read.o: src/board_read.c
+	gcc -Wall -Werror -c src/board_read.c -o build/src/board_read.o
+
+bin/chessviz-test: build/test/main.o build/test/test.o build/src/board.o build/src/board_print_plain.o build/src/board_read.o
+	gcc -Wall -Werror build/test/main.o build/test/test.o build/src/board.o build/src/board_print_plain.o build/src/board_read.o -o bin/chessviz-test
+
+build/test/main.o: test/main.c
+	gcc -I thirdparty -Wall -Werror -c test/main.c -o build/test/main.o
+
+build/test/test.o: test/test.c
+	gcc -I thirdparty -Wall -Werror -c test/test.c -o build/test/test.o
+
 
 .PHONY: clean
 
-run:
-	./bin/main
+run: 
+	bin/chessviz
 
 clean:
-	rm -rf build/*o ./bin/main
+	rm -rf build/*.o
--- src/main.c
+++ src/main.c
@@ -1,48 +1,46 @@
 #include "board.h"
 #include "board_print_plain.h"
+#include "board_read.h"
 #include <stdio.h>
 
 char input[7];
 int X1, X2, Y1, Y2;
-char desk[8][8] = {{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
-                   {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                   {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                   {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
+char board[8][8] = {{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
 
 int main()
 {
     int status = 0;
 
-    printf("     Шахматы\n");
-    printf("Пример обычного хода: E2-E4\n");
-
-    printdesk();
+    printboard();
 
     while (1) {
         printf("Белые (Большие буквы):");
-        scanan(1);
+        scan(1);
         move();
-        printdesk();
+        printboard();
         status = checkWIn(1);
         if (status != 0) {
             break;
         }
 
         printf("Черные (Маленькие буквы):");
-        scanan(2);
+        scan(2);
         move();
-        printdesk();
+        printboard();
         status = checkWIn(2);
         if (status != 0) {
             break;
         }
     }
 
-    printf("Игрок %d выиграл\n", status);
+    printf("                          Игрок %d выиграл!\n", status);
 
     return 0;
 }
